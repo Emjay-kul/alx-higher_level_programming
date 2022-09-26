6faf04ebea050127d3937f906c2942288eddd9e2
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-09-26 09:36:44 -0700

    f

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for var in range(ord('z'), ord('a') - 1, -1):
-    if var % 2 == 1:
-        var -= (ord('a') - ord('A'))
-        print("{:c}".format(var), end='')
+    if var % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print("{}".format(chr(i - diff)), end='')
