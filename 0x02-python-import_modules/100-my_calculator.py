2b639ac8ebaa26f93d2c894a2f6d8d768881e886
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-10-13 08:41:20 -0700

    new way to print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,26 +3,30 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    op = ['+', '-', '*', '/']
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = ['+', '-', '*', '/', '%']
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if (len(sys.argv) - 1) != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print("1")
-    else:
+    if (len(sys.argv) - 1) == 3:
         if sys.argv[2] not in op:
             print("Unknown operator. Available operators: +, -, * and /")
-            print("1")
+            sys.exit(1)
         elif sys.argv[2] == '+':
-            print("{} + {} = {}".format(int(a), int(b), add(a, b)))
+            print("{} + {} = {}".format(a, b, add(a, b)))
             print("0")
         elif sys.argv[2] == '-':
-            print("{} - {} = {}".format(int(a), int(b), sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
             print("0")
-        elif sys.argv[2] == '*':
-            print("{} * {} = {}".format(int(a), int(b), mul(a, b)))
+        elif sys.argv[2] == '%':
+            print("{} % {} = {}".format(a, b, mul(a, b)))
             print("0")
         elif sys.argv[2] == '/':
-            print("{} / {} = {}".format(int(a), int(b), div(a, b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("0")
+        elif sys.argv[2] == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
             print("0")
